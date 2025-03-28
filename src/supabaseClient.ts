821ea9bebe1b8de9f3a76e91966cbdf61f56ee38
Pedro Santayana <pedro.santayana@levty.com>
2025-03-28 18:48:02 -0300

    a

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const VITE_SUPABASE_URL="https://qftqouhezpcngcjtgjfi.supabase.co"
+const VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdHFvdWhlenBjbmdjanRnamZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI1MDY3NzQsImV4cCI6MjA1ODA4Mjc3NH0.sFxh7YrgoHMvQuoIx2IQg_RiWsYMfgQbqlGBAfqPA-E"
+
+const supabaseUrl = VITE_SUPABASE_URL
+const supabaseAnonKey = VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
